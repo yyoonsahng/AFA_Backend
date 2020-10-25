b69ceb2338e09ad2971d3a9f4712be7031a0bd4b
yyoonsahng <dbstkd960@konkuk.ac.kr>
2020-10-26 04:19:55 +0900

    check

--- grad_project/api_user/urls.py
+++ grad_project/api_user/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'api_user'
 urlpatterns = [
-    path('', views.UserView.as_view()),  # User에 관한 API를 처리하는 view로 Request를 넘김
+    path('', views.UserView.as_view()),  #User에 관한 API를 처리하는 view로 Request를 넘김
 ]
